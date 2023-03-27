a227f7f875bbe4d27623e7a85755968875ea3f69
ryansummerlin <ryansummerlin777@gmail.com>
2023-03-26 19:00:02 -0700

    created the server and told it to listen to requests on port 5000

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,46 @@
 function firstStep(input) {
-  // Your code here
+  return input.split('&');
 }
 
 function secondStep(input) {
-  // Your code here
+  return input.map(str => str.split('='));
 }
 
 function thirdStep(input) {
-  // Your code here
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      input[i] = input[i].map(str => str.replace(/\+/g, ' '));
+    }
+  }
+  return input;
 }
 
 function fourthStep(input) {
-  // Your code here
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+        input[i][j] = decodeURIComponent(input[i][j]);
+    }
+  }
+  return input;
 }
 
 function fifthStep(input) {
-  // Your code here
+  let obj = {};
+
+  for (let i = 0; i < input.length; i++) {
+    let pair = input[i];
+    obj[pair[0]] = pair[1];
+  }
+
+  return obj;
 }
 
 function parseBody(str) {
-  // Your code here
+  let temp = firstStep(str);
+  temp = secondStep(temp);
+  temp = thirdStep(temp);
+  temp = fourthStep(temp);
+  return fifthStep(temp);
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -5,7 +5,15 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here
+const http = require('http');
+
+server = http.createServer((req, res) => {
+
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Succesfully started the server on port', port));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
